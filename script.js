31cc16c50dd01c6b52f3b5a6b55cf30072d8a5bc
Engerberth Reyes <engerberthr73@gmail.com>
2026-04-21 10:19:22 -0400

    Arreglando Bug

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const cambiarModo = () => {
   const body = document.querySelector(".body");
   const checkbox = document.querySelector(".palanca");
-  const texto = document.querySelector(".texto-estado");
 
   if (!checkbox.checked) {
     body.classList.add("modo-claro");
-    texto.textContent = "El modo claro está activado";
   } else {
     body.classList.remove("modo-claro");
-    texto.textContent = "El modo oscuro está activado";
   }
 };
