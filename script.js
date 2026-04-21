f7e7820ebf27250cd71b1e250feb4a3341b28f12
Engerberth Reyes <engerberthr73@gmail.com>
2026-04-21 10:43:19 -0400

    Agreganado Sol y Luna

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const cambiarModo = () => {
   const body = document.querySelector(".body");
   const checkbox = document.querySelector(".palanca");
+  const icono = document.querySelector(".icono-clima");
 
   if (!checkbox.checked) {
+    icono.src = "Imagenes/Imagenes Enlazadas/Sol.svg";
     body.classList.add("modo-claro");
   } else {
+    icono.src = "Imagenes/Imagenes Enlazadas/Moon.svg";
     body.classList.remove("modo-claro");
   }
 };
